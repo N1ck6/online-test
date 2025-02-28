60aca757a85d60c0e2af8dd209c7086723fa0525
Nicki <91270829+N1ck6@users.noreply.github.com>
2025-03-01 01:08:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-console.log("Starting the application...");
-
-const apiKey = process.env.APIKEY;
-
-if (apiKey) {
-  // The API key comes from the GitHub secret and is injected as an environment variable.
-  console.log("APIKEY is:", apiKey);
-} else {
-  console.log("No APIKEY provided.");
-}
-
-console.log("Application finished.");
+const apiKey = '__API_KEY__';
+console.log("APIKEY is:", apiKey);
