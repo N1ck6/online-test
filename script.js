3f8ccca78115a3c94af5fd4362f568387dd9a67d
Nicki <91270829+N1ck6@users.noreply.github.com>
2025-03-01 01:12:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const apiKey = '__API_KEY__';
-console.log("APIKEY is:", apiKey);
+console.log("Starting the application...");
+
+const apiKey = process.env.APIKEY;
+
+if (apiKey) {
+  console.log("APIKEY is:", apiKey);
+} else {
+  console.log("No APIKEY provided.");
+}
+
+console.log("Application finished.");
